07148fc60e9abb2405c4747c86ec9916fb350354
FuRy30 <adam.jan.duda.krk@gmail.com>
2023-09-04 06:18:39 +0200

    Files Controller Url Fix

--- config.ts
+++ config.ts
@@ -7,13 +7,13 @@ interface Config {
 const DevConfig: Config = {
     GraphQLUrl: 'https://localhost:44384/graphql/',
     TutorsMicroservice_GraphQLUrl: 'https://localhost:44378/graphql/',
-    TutorsMicroservice_FilesUrl: 'https://localhost:44378/files/'
+    TutorsMicroservice_FilesUrl: 'https://localhost:44378/files'
 };
 
 const ProdConfig: Config = {
     GraphQLUrl: 'https://competifymarketplacebackendtestserver.azurewebsites.net/graphql/',
     TutorsMicroservice_GraphQLUrl: 'https://competifymentorbackendtestserver.azurewebsites.net/graphql/',
-    TutorsMicroservice_FilesUrl: 'https://competifymentorbackendtestserver.azurewebsites.net/files/'
+    TutorsMicroservice_FilesUrl: 'https://competifymentorbackendtestserver.azurewebsites.net/files'
 };
   
 export const Config = process.env.NODE_ENV === 'production' ? ProdConfig : DevConfig;
