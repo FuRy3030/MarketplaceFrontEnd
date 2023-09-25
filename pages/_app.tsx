2890188be1999304e17c5901da0bf7519df21b78
FuRy30 <adam.jan.duda.krk@gmail.com>
2023-09-25 06:29:53 +0200

    Cookies Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -55,30 +55,22 @@ function MyApp({ Component, pageProps }: AppProps) {
                         <meta property="og:url" content="https://www.competify.pl" />
                         <meta property="og:type" content="website" />
                         <link rel="icon" href="/logo/favicon.ico" />
-                        <script>
-                        {`
-                            !function(f,b,e,v,n,t,s)
-                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                            n.queue=[];t=b.createElement(e);t.async=!0;
-                            t.src=v;s=b.getElementsByTagName(e)[0];
-                            s.parentNode.insertBefore(t,s)}(window, document,'script',
-                            'https://connect.facebook.net/en_US/fbevents.js');
-                            fbq('init', '146653598423875');
-                            fbq('track', 'PageView');
-                        `}
-                        </script>
-                        <noscript>
-                            <img
-                                height="1"
-                                width="1"
-                                style={{ display: 'none' }}
-                                src="https://www.facebook.com/tr?id=146653598423875&ev=PageView&noscript=1"
-                            />
-                        </noscript>
-                        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QXVP0FWBDK"></script>
-                        <script>
+                        <script type="text/plain" data-cookieconsent="marketing">
+                            {`
+                                !function(f,b,e,v,n,t,s) 
+                                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; 
+                                n.queue=[];t=b.createElement(e);t.async=!0;
+                                t.src=v;s=b.getElementsByTagName(e)[0]; 
+                                s.parentNode.insertBefore(t,s)}(window, document,'script',  
+                                'https://connect.facebook.net/en_US/fbevents.js');
+                                fbq('init', '146653598423875');
+                                fbq('track', 'PageView');
+                            `}
+                        </script> 
+                        <script type="text/plain" data-cookieconsent="marketing" async src="https://www.googletagmanager.com/gtag/js?id=G-QXVP0FWBDK"></script>
+                        <script type="text/plain" data-cookieconsent="marketing">
                         {`
                             window.dataLayer = window.dataLayer || [];
                             function gtag(){dataLayer.push(arguments);}
